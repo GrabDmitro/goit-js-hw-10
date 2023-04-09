dfd07b6ffe3ff6b1c9ca4839387f3cf93c9c2651
GrabDmitro <d_grab2001@ukr.net>
2023-04-09 11:46:32 +0300

    delConsLog

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import fetchCountry from './fetchCountries.js';
 import renderCountry from './renderCountry';
 import { debounce } from 'lodash';
-import Notiflix from 'notiflix';
+
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
